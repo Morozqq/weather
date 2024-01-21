6ab86bef1755e989015c551d0987b4e836803cfa
Morozqq <aslan.dzheleubaj.04@gmail.com>
2024-01-21 21:45:57 +0600

    Country API

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/country", (req, res) => {
+    res.sendFile(__dirname + "/country.html");
+});
+
 // Function to get weather data by city
 function getWeatherByCity(city, res) {
     const url =
@@ -75,7 +79,45 @@ function displayWeatherData(res, weatherdata) {
     });
 }
 
-// Routte for city weather
+// Function to get country data by name
+function getCountryData(countryName, res) {
+    const apiUrl = `https://restcountries.com/v3.1/name/${countryName}`;
+
+    // Make an HTTP GET request
+    const req = https.get(apiUrl, (response) => {
+        let data = "";
+
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+
+        response.on("end", () => {
+            try {
+                const countryData = JSON.parse(data)[0]; // Assuming the first result if there are multiple matches
+                displayCountryData(res, countryData);
+            } catch (error) {
+                console.error("Error parsing country data:", error.message);
+                res.status(500).json({ error: "Internal Server Error" });
+            }
+        });
+    });
+
+    // Handle errors during the HTTP request
+    req.on("error", (error) => {
+        console.error("Error fetching country data:", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    });
+
+    req.end();
+}
+
+// Function to display country data
+function displayCountryData(res, countryData) {
+    console.log("Country Data:", countryData);
+    res.json(countryData);
+}
+
+// Route for city weather
 app.post("/weather", (req, res) => {
     const city = req.body.city;
     getWeatherByCity(city, res);
@@ -86,6 +128,12 @@ app.post("/latlon", (req, res) => {
     const lat = req.body.lat;
     const lon = req.body.lon;
     getWeatherByLatLon(lat, lon, res);
+});
+
+// Route for country data
+app.post("/countrydata", (req, res) => {
+    const countryName = req.body.countryName;
+    getCountryData(countryName, res);
 });
 
 app.listen(3000, () => {
